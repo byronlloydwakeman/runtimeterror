b3fc7ef949bfffaf89cc22bfef534e9a3058c4df
Byron Lloyd Wakeman <byzstorm1103@gmail.com>
2024-03-06 10:22:33 +0000

    Updated passwordgenerator imports.

--- pages/passwordgenerator.jsx
+++ pages/passwordgenerator.jsx
@@ -9,8 +9,8 @@ import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
 import VisibilityOffOutlinedIcon from '@mui/icons-material/VisibilityOffOutlined';
 import styles from './passwordgenerator.module.css';
-import Navbar from '../../components/Navbar';
-import NavbarBottom from '../../components/NavbarBottom';
+import Navbar from '../components/Navbar';
+import NavbarBottom from '../components/NavbarBottom';
 import { platform } from 'os';
 import Snackbar from '@mui/material/Snackbar';
 
