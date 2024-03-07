7d70c789adaaebb258a7bbf5f6786cf25c5fb5e1
Tharan Patel <tharan451@gmail.com>
2024-03-07 20:07:26 +0000

    converted hard-coded challenge list to be a json file that is mapped through by the challenge page.

--- pages/challenges.jsx
+++ pages/challenges.jsx
@@ -9,6 +9,7 @@ import Navbar from '../components/Navbar';
 import NavbarBottom from '../components/NavbarBottom';
 import styles from './challenges.module.scss';
 import Link from 'next/link';
+import challenge_list from '../public/challenges.json';
 
 export default function Challenges() {
   // STYLING
@@ -28,44 +29,56 @@ export default function Challenges() {
   };
   // STYLING
 
-  const card = (
-    <React.Fragment>
-      <CardContent>
-        <Typography sx={{ fontSize: 16 }} color="text.secondary">
-          Week 1
-        </Typography>
-        <Typography sx={{ fontSize: 22 }} component="div">
-          Password Generator
-        </Typography>
-        <Typography sx={{ mb: 1.5, fontSize: 12 }} color="text.secondary">
-          UI • Validation • Testing • Hosting
-        </Typography>
-        <Typography variant="body2">
-          A user interface where a user can easily generate a password string.
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Link href="/passwordgenerator">
-          <Button size="small" sx={buttonStyle.Button}>
-            Visit Challenge
-          </Button>
-        </Link>
-      </CardActions>
-    </React.Fragment>
-  );
+  const challengeList = challenge_list.challenges;
+  const challenges = [];
+
+  challengeList.forEach((challenge, index) => {
+    challenges.push(
+      <React.Fragment>
+        <CardContent>
+          <Typography sx={{ fontSize: 16 }} color="text.secondary">
+            {challenge.week}
+          </Typography>
+          <Typography sx={{ fontSize: 22 }} component="div">
+            {challenge.title}
+          </Typography>
+          <Typography sx={{ mb: 1.5, fontSize: 12 }} color="text.secondary">
+            {challenge.tags.map(function (tag, i) {
+              return <span>{tag} • </span>;
+            })}
+          </Typography>
+          <Typography variant="body2">{challenge.description}</Typography>
+        </CardContent>
+        <CardActions>
+          <Link href={challenge.link}>
+            <Button size="small" sx={buttonStyle.Button}>
+              Visit Challenge
+            </Button>
+          </Link>
+        </CardActions>
+      </React.Fragment>
+    );
+  });
+
   return (
     <div>
       <Navbar />
       <div className={styles.main_container}>
         <h1 className={styles.page_title}>Challenges</h1>
-        <Box sx={{ width: 300 }}>
-          <Card
-            variant="outlined"
-            sx={{ padding: '15px', borderRadius: '20px' }}
-          >
-            {card}
-          </Card>
-        </Box>
+        <div className={styles.challenge_container}>
+          {challenges.map(function (challenge) {
+            return (
+              <Box sx={{ width: 300 }}>
+                <Card
+                  variant="outlined"
+                  sx={{ padding: '15px', borderRadius: '20px' }}
+                >
+                  {challenge}
+                </Card>
+              </Box>
+            );
+          })}
+        </div>
       </div>
       <NavbarBottom />
     </div>
