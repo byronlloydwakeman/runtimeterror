afa6fd87b0a8a8e1ac2f6f8f1fe53a54122d7bd5
Byron Lloyd Wakeman <byzstorm1103@gmail.com>
2024-03-14 12:34:00 +0000

    Created Preloader component
    Added Dependencies.
    Created Preloader on homepage entry.

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,39 @@
+import { useEffect, useState } from 'react';
 import Navbar from '../components/Navbar';
 import NavbarBottom from '../components/NavbarBottom';
 import styles from './index.module.scss';
+import Preloader from '../components/Preloader/preloader';
+import { AnimatePresence } from 'framer-motion';
 
-const Home = () => (
-  <div className={styles.main_container}>
-    <Navbar />
-    <div className={styles.sub_container}>
-      <h1 className={styles.page_title}>Homepage</h1>
+export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect( () => {
+    (
+      async () => {
+          setTimeout( () => {
+            setIsLoading(false);
+            document.body.style.cursor = 'default'
+            window.scrollTo(0,0);
+          }, 2500)
+      }
+    )()
+  }, [])
+
+  return (
+    <div className={styles.main_container}>
+      <AnimatePresence>
+      {
+        isLoading && <Preloader/>
+      }
+      </AnimatePresence>
+
+      <Navbar />
+      <div className={styles.sub_container}>
+        <h1 className={styles.page_title}>Homepage</h1>
+      </div>
+      <NavbarBottom />
     </div>
-    <NavbarBottom />
-  </div>
-);
+  )
+}
 
-export default Home;
