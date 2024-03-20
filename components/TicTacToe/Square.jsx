67798bce871f077c67bb11234db781ef46cbc57a
tharanpatel <tharan451@gmail.com>
2024-03-20 15:55:23 +0000

    Implemented game logic

--- components/TicTacToe/Square.jsx
+++ components/TicTacToe/Square.jsx
@@ -1,33 +1,32 @@
-import { motion } from "framer-motion";
-import styles from "./square.module.scss";
+import { motion } from 'framer-motion';
+import styles from './square.module.scss';
 
-export const Square = ({index, player1, playerHook, squares}) => {
-    function clicked() {
-        // Change go
-        playerHook(player => !player);
-        if (!squares[index])
-        {
-            squares[index] = player1 == true ? "❌" : "⭕";
-        }
+export const Square = ({ index, player1, playerHook, squares }) => {
+  function clicked() {
+    // Change go
+    playerHook((player) => !player);
+    if (!squares[index]) {
+      squares[index] = player1 == true ? '❌' : '⭕';
     }
+  }
 
-    return (
-        <motion.button
-            onClick={clicked}
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9 }}
-            className={styles.square}
-            style={{ overflow: 'hidden' }}
-        >
-            <motion.span
-            style={{ display: 'inline-block', overflow: 'hidden' }}
-            initial={{ opacity: 0, scale: 0.5 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{ type: 'spring', stiffness: 300, damping: 20 }}
-            className={squares[index] == "❌" ? styles.X : styles.O}
-            >
-            {squares[index]}
-            </motion.span>
-        </motion.button>
-    )
-}
+  return (
+    <motion.button
+      onClick={clicked}
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.9 }}
+      className={styles.square}
+      style={{ overflow: 'hidden' }}
+    >
+      <motion.span
+        style={{ display: 'inline-block', overflow: 'hidden' }}
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ type: 'spring', stiffness: 300, damping: 20 }}
+        className={squares[index] == '❌' ? styles.X : styles.O}
+      >
+        {squares[index]}
+      </motion.span>
+    </motion.button>
+  );
+};
--- pages/tictactoe.jsx
+++ pages/tictactoe.jsx
@@ -1,22 +1,80 @@
-import React, { useState } from "react";
-import { Square } from "../components/TicTacToe/Square.jsx";
-
-import styles from "./tictactoe.module.scss"
-import Navbar from "../components/Navbar.jsx";
+import React, { useEffect, useState } from 'react';
+import { Square } from '../components/TicTacToe/Square.jsx';
+import styles from './tictactoe.module.scss';
+import Navbar from '../components/Navbar.jsx';
+import NavbarBottom from '../components/NavbarBottom';
+import winningArrays from '../public/winningSquares.json';
 
 export default function TicTacToe() {
+  const [squares, setSquares] = useState(['', '', '', '', '', '', '', '', '']);
+  const [player1, setPlayer1] = useState(true);
+  const [winner, setWinner] = useState('Undecided');
+  const [crossWinCount, setCrossWinCount] = useState(0);
+  const [naughtsWinCount, setNaughtsWinCount] = useState(0);
+  const winningCrossArrays = winningArrays.winningCrossArrays;
+  const winningNaughtsArrays = winningArrays.winningNaughtsArrays;
 
-    const [squares, setSquares] = useState(["", "", "", "", "", "", "", "", ""]);
-    const [player1, setPlayer1] = useState(true);
+  useEffect(() => {
+    winningCrossArrays?.map((winningArray) => {
+      const firstIndex = winningArray[1][0];
+      const secondIndex = winningArray[1][1];
+      const thirdIndex = winningArray[1][2];
 
-    return (
-        <div className={styles.container}>
-          <Navbar />
-            <div className={styles.square_container}>
-                {Array.from(squares).map((el, ind) => (
-                    <Square index={ind} player1={player1} playerHook={setPlayer1} squares={squares}/>
-                ))}
-            </div>
+      if (
+        squares[firstIndex] == winningArray[0][firstIndex] &&
+        squares[secondIndex] == winningArray[0][secondIndex] &&
+        squares[thirdIndex] == winningArray[0][thirdIndex]
+      ) {
+        setWinner('crosses');
+        setCrossWinCount((crossWinCount) => crossWinCount + 1);
+      }
+    });
+
+    winningNaughtsArrays?.map((winningArray) => {
+      const firstIndex = winningArray[1][0];
+      const secondIndex = winningArray[1][1];
+      const thirdIndex = winningArray[1][2];
+
+      if (
+        squares[firstIndex] == winningArray[0][firstIndex] &&
+        squares[secondIndex] == winningArray[0][secondIndex] &&
+        squares[thirdIndex] == winningArray[0][thirdIndex]
+      ) {
+        setWinner('naughts');
+        setNaughtsWinCount((naughtsWinCount) => naughtsWinCount + 1);
+      }
+    });
+
+    if (!squares.includes('')) {
+      setWinner('Draw');
+    }
+  }, [player1, squares, winningCrossArrays, winningNaughtsArrays]);
+
+  const resetBoard = () => {
+    setSquares(['', '', '', '', '', '', '', '', '']);
+  };
+
+  return (
+    <div>
+      <Navbar />
+      <div className={styles.container}>
+        <div className={styles.square_container}>
+          {Array.from(squares).map((el, ind) => (
+            <Square
+              key={ind}
+              index={ind}
+              player1={player1}
+              playerHook={setPlayer1}
+              squares={squares}
+            />
+          ))}
         </div>
-    )
+        <h1>Winner is: {winner}</h1>
+        <button onClick={resetBoard}>Reset Board</button>
+        <div>Cross win count: {crossWinCount}</div>
+        <div>Naughts win count: {naughtsWinCount}</div>
+      </div>
+      <NavbarBottom />
+    </div>
+  );
 }
